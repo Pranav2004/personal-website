0311a54a85951314ee12d7295bac1f623370ba9a
Pranavjd <jadavpranav1997@gmail.com>
2025-01-09 15:19:28 -0500

    Install Nodejs Packages

---
+++ main/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "20vh" }}>
+      <h1>Pranav Jadav</h1>
+      <p>Web Developer | Full Stack Engineer</p>
+      <p>
+        <a href="https://github.com/pranavjadav" target="_blank">GitHub</a> |{" "}
+        <a href="https://linkedin.com/in/pranavjadav" target="_blank">LinkedIn</a>
+      </p>
+    </div>
+  );
+}
